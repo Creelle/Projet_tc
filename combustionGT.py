e49c2fb001c4c4e1550fb59a8fa11556530c7bf2
RobbeCreelle <44603306+Creelle@users.noreply.github.com>
2020-09-29 15:05:55 +0200

    Revert "Update combustionGT.py"
    This reverts commit e5570bfb40d358d9e5f3bd9086ab40f6259782c5.

--- combustionGT.py
+++ combustionGT.py
@@ -1,42 +1,16 @@
 
 from thermochem import janaf
-from thermochem import combustion
-from thermochem import burcat
-from numpy import*
-from scipy.linalg import*
-import matplotlib.pyplot as plt
-db = janaf.Janafdb();
-burcatdb = burcat.Elementdb()
-# mix = burcatdb.getmixturedata([("O2 REF ELEMENT", 20.9476), ("N2 REF ELEMENT",78.084), ("CO2", 0.0319), ("AR REF ELEMENT", 0.9365), ])
-
 import numpy as np;
-
 import GT_arguments as GT_arg;
 
-def cp_Iconstants(M,T_0,T_1):
-    #cp = A + BT + CT^2 + DT^3 (cp évolue en T^3 (sauf au début => à débattre dans le rapport : faire un calcul d'erreur))
-    n=1000 # n est arbitraire : j'ai pris n points pour avoir le plus de précision possible
-    #db.getphasedata est différente pour l'oxygène que pour les autres gazs donc je dois mettre un if.
-    if M=='O2' or M=='N2':
-        chim = db.getphasedata(M,'g');
-        T = linspace(T_0,T_1,n) #je divise en espaces réguliers l intervalle de températures
-        cp_values = zeros(n)
-        for i in range (0,n):
-            cp_values[i] = chim.cp(T[i])  #je prends toutes les valeurs de cp correspondant aux températures
-        heat_const = polyfit(T,cp_values,3) #je trouve les constantes A,B,C,D de de l'interpolation polynomiale grâce à polyfit
-        I = heat_const[3]*(T_1-T_0) + (1/2)*heat_const[2]*(T_1**2-T_0**2) + (1/3)*heat_const[1]*(T_1**3-T_0**3) + (1/4)*heat_const[0]*(T_1**4-T_0**4)
-        #j'intègre cp sur l'intervalle de température
-    else:
-        chim = db.getphasedata(M,phase ='g');
-        T = linspace(T_0,T_1,n)
-        cp_values = zeros(n)
-        for i in range (0,n):
-            cp_values[i] = chim.cp(T[i])
-        heat_const = polyfit(T,cp_values,3)
-        I = heat_const[3]*(T_1-T_0) + (1/2)*heat_const[2]*(T_1**2-T_0**2) + (1/3)*heat_const[1]*(T_1**3-T_0**3) + (1/4)*heat_const[0]*(T_1**4-T_0**4)
-        #je retourne l'intégrale
-    return I
-print("O2 :", cp_Iconstants('O2',273,600))
+db = janaf.Janafdb();
+O2 = db.getphasedata('O2','g');
+N2 = db.getphasedata('N2','g');
+CO2 = db.getphasedata('CO2',phase ='g');
+H2O = db.getphasedata('H2O',phase ='g');
+CH4=db.getphasedata('CH4',phase ='g');
+# mix = burcatdb.getmixturedata([("O2 REF ELEMENT", 20.9476), ("N2 REF ELEMENT",78.084), ("CO2", 0.0319), ("AR REF ELEMENT", 0.9365), ])
+
 
 def combustionGT(comb_input):
     """
@@ -47,20 +21,46 @@ def combustionGT(comb_input):
      plots graphs if input argument DISPLAY = true (<=> DISPLAY=1)
 
      INPUTS (some inputs can be dependent on others => only one of these 2 can
-             be activated) Refer to Fig 3.1 from reference book (in english)0
-"""
-    #fuel=burcatdb.getelementdata("CH4   RRHO")  # choose the right fuel
-    #combObject=combustion.SimpleCombustor(fuel,1/lambda_comb,burcatdb)
+             be activated) Refer to Fig 3.1 from reference book (in english)
+     P_E = electrical power output target [kW]
+     OPTIONS is a structure containing :
+       -options.T_ext [°C] : External temperature
+       -options.r     [-] : Compression ratio
+                            chamber
+       -options.T_3   [°C] : Temperature after combustion (before turbine)
+       -option.eta_PiC[-] : Intern polytropic efficiency (Rendement
+                            polytropique interne) for compression
+       -option.eta_PiT[-] : Intern polytropic efficiency (Rendement
+                            polytropique interne) for expansion
+    """
+
+    lambda_comb = comb_input.lambda_comb
+    x_O2a = comb_input.x_O2a
+    x_N2a = comb_input.x_N2a
+    coeff = x_N2a/x_O2a
+    T_in  = comb_input.T_in +288.15 #[K]
+    h_in  = comb_input.h_in*1000 #J/kgair
+    h_in_ch4 =h_in*0.028*1000/0.21# J/kg_air *(kg_air/mol_air)*mol_air/mol_ch4 = J/mol_ch4
+    LHV   =comb_input.LHV*1000 #J/kg_ch4]
+    LHV_mol = LHV*0.016 #[J/mol_CH4]
+    T_ref = 288.15 #[K]
+
+    # calcul des cp : cec doit encore changer lorsqu on va integrer
+    cp_O2 = O2.cp(T_in)
+    cp_CO2 = CO2.cp(T_in)
+    cp_H2O = H2O.cp(T_in)
+    cp_N2 = N2.cp(T_in)
+    cp_CH4 = CH4.cp(T_in)
+    # CH4 + 2 *lambda * (O2 + coeff*N2) <=> CO2+2*H2O+ 2*lambda*coeff*N2 + 2*(lambda-1)*O2
+    T_out =(LHV_mol+ 2*lambda_comb*h_in_ch4 + cp_CH4*(T_in-T_ref))/(cp_CO2+2*cp_H2O+2*(lambda_comb-1)*cp_O2+2*lambda_comb*coeff*cp_N2)
 
     # donne une liste avec les coeff stochio pour les produits de la combustion dans cette ordre N2 - CO2 - H2O - O2
-    #coeff_stochio = combustion.balance(fuel,1,1/lambda_comb)[1]
-    #print(combustion.balance(fuel,1,1/lambda_comb))
-    #list_coeff = np.asarray(list(coeff_stochio.values()))
-
-    #total_n = sum(list_coeff) # nombre de moles total
-    #molar_conc = list_coeff/total_n # concentration des elements mol_co2/mol_t
-    #molar_mass = np.array([0.028,0.044,0.018,0.032]) #kg/mol N2- CO2 - H2O - O2
-    #Mm_af = sum(molar_conc*molar_mass) #somme ponderé des masse molaire
+    coeff_stochio = np.array([2*lambda_comb*coeff,1,2,2*(lambda_comb-1)]) # N2- CO2 - H2O - O2
+    print(coeff_stochio)
+    total_n = sum(coeff_stochio) # nombre de moles total
+    molar_conc = coeff_stochio/total_n # concentration des elements mol_co2/mol_t
+    molar_mass = np.array([0.028,0.044,0.018,0.032]) #kg/mol N2- CO2 - H2O - O2
+    Mm_af = sum(molar_conc*molar_mass) #somme ponderé des masse molaire
 
     #  OUTPUTS
     #  R_f   : ideal gas constant (R*) for specific gas (R/Mm_f) [kJ/kg/K]
@@ -70,37 +70,15 @@ def combustionGT(comb_input):
     #  m_H2Of : mass fraction of water steam in exhaust gases [-]
     #  T_out  : outlet gas temperature [K]
 
-    #outputs = GT_arg.comb_output();
-    #outputs.T_out = combObject.adiabatic_flame_temp(273.15+T_in); #[K]
-    #outputs.R_f = 8.31/1000/Mm_af # [kJ/kg/K]
-    #outputs.m_N2f,outputs.m_CO2f,outputs.m_H2Of,outputs.m_O2f = molar_conc*molar_mass/Mm_af #[-]
-    Mm_CH4 = 0.016; Mm_O2 = 0.032; Mm_N2 = 0.028; Mm_H2O = 0.018; Mm_CO2 = 0.044
-    T_out =  1400
-    m_a1 = 5
-    iter = 1
+    outputs = GT_arg.comb_output();
+    outputs.T_out = T_out
+    outputs.R_f = 8.31/1000/Mm_af # [kJ/kg/K]
+    outputs.m_N2f,outputs.m_CO2f,outputs.m_H2Of,outputs.m_O2f = molar_conc*molar_mass/Mm_af #[-]
 
-    while iter <= 1000 or error > 0.1 :
-        Num =
-        Den1 =
-        Den2 = (lambda_comb*m_a1 + 1)
-        T_out_final = 1 + (Num/Den) - h_f0
-        iter = iter + 1
-        error = T_out_final - T_out
-        T_out = T_out_final
-        print(T_out)
-    print(iter,T_out)
-
-
-
-
-
-    outputs = 0;
-    #outputs.T_out = T_out
-    #outputs.R_f = 8.31/1000 # [kJ/kg/K]
-    #outputs.m_N2f,outputs.m_CO2f,outputs.m_H2Of,outputs.m_O2f = 3 #[-]
     return outputs;
+
 ### test
-#print()
-#sol =combustionGT(GT_arg.comb_input)
-#print(sol.T_out)
-#combustionGT(GT_arg.comb_input(lambda_comb = 2))
+
+sol =combustionGT(GT_arg.comb_input(lambda_comb = 2))
+print(sol.T_out)
+print()
